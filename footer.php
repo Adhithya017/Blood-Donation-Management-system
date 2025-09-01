7fa3fd23fe74211bcfcfda8131678dfd426bfad2
Adhithya017 <adithyaachar686@gmail.com>
2025-09-01 16:14:36 +0530

    Initial commit of the blood donation system

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+
+echo '<div id="upper" class="col-12">
+        <div style="padding: 40px 70px 40px 70px; color: white;overflow: auto;">
+            <div class="col-3" style="float: left; height: 250px; padding: 10px 40px 10px 20px; font-size: 15px; color: rgb(171, 175, 181);overflow: hidden;text-overflow: ellipsis;">
+                <h5>ABOUT US</h5><p>A non-profit organization committed to connecting blood donors with hospitals and patients to ensure timely access to life-saving blood donations.</p>
+            </div>
+
+            <div class="col-3" style="float: right; height: 250px; padding: 10px;overflow: hidden;text-overflow: ellipsis;">
+                <h5>SOCIAL ICONS</h5><p><a href="https://www.facebook.com" class="fa fa-facebook"></a><a href="https://www.twitter.com" class="fa fa-twitter"></a><a href="https://www.google.com" class="fa fa-google"></a><a href="https://www.youtube.com" class="fa fa-youtube"></a><a href="https://www.instagram.com" class="fa fa-instagram"></a></p>
+            </div>
+
+            <div class="col-3" style="float: right; height: 250px; padding: 10px;overflow: hidden;text-overflow: ellipsis;">
+                <h5>HELP</h5> <a style="text-decoration:none" href="register.php"><button style="background-color:lightgrey;padding:5px">Donate Blood</button></a>
+            </div>
+
+            <div class="col-3" style="float: right; height: 250px; padding: 10px; font-size: 15px; color: rgb(171, 175, 181);overflow: hidden;text-overflow: ellipsis;">
+                <h5>KEEP IN TOUCH</h5><p>Blood Connect<br><br>(+91) 9876543210 &ensp; info@bloodconnect.in</p>
+            </div>
+        </div>
+    </div>
+    <div id="lower" class="col-12">
+        All Rights Reserved.
+    </div>';
+
+?>
